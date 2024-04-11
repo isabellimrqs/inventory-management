eb5fc26edb91b8331e70e2f1796cbb939bb7de53
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2024-04-11 16:37:32 -0300

    delete and search

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import { ProductProvider } from './ProductContext';
 import ProductsTable from './components/ProductsTable'
+import AddProducts from './components/AddProducts';
 
 function App() {
   return (
@@ -13,7 +14,9 @@ function App() {
           <NavBar/>
           <div className="row">
             <div className="col-sm-10 col-xm-12 mr-auto ml-auto mt-4 mb-4">
-          <ProductsTable/>
+              <Route exact path = "/" component={ProductsTable}/> {/*taking too long to load the page, comment if present */}
+              
+              <Route exact path = "/addproduct" component={AddProducts}/>
             </div>
           </div>
           </ProductProvider>
--- front/src/components/ProductsTable.js
+++ front/src/components/ProductsTable.js
@@ -1,9 +1,41 @@
 import react, { useContext, useEffect } from 'react'
 import { Table } from 'react-bootstrap';
 import {ProductContext} from '../ProductContext'
+import ProductsRow from './ProductsRow'
+import { useHistory } from 'react-router-dom';
 
 const ProductsTable = () => {
     const [products, setProducts] = useContext(ProductContext)
+
+    let history = useHistory()
+
+    
+
+    const handleDelete  = (id) => {
+        fetch("http://127.0.0.1:8000/product/" + id,{
+            method: "DELETE",
+            headers: {
+                accept: 'application/json'
+            }
+        })
+            .then(resp => {
+            return resp.json()
+        })
+        .then(result =>{
+            if (result.status === 'ok'){
+                const filteredProducts = products.data.filter((product) => product.id !== id);
+                setProducts({data: [...filteredProducts]})
+                alert("Product deleted")
+            }else {
+                alert("Product deletion failed")
+            }
+        })
+    }
+
+    const handleUpdate = (id) => {
+        console.log("Hello World")
+        
+    }
 
 
     useEffect(()=>{
@@ -31,14 +63,23 @@ const ProductsTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-
-                    
-                        
-                </tbody>
-            </Table>
+                {products.data.map((product) => (
+                        <ProductsRow
+                            id = {product.id}
+                            name = {product.name}
+                            quantity_in_stock = {product.quantity_in_stock}
+                            quantity_sold = {product.quantity_sold}
+                            unit_price = {product.unit_price}
+                            revenue = {product.revenue}
+                            key={product.id}
+                            handleDelete = {handleDelete}
+                            handleUpdate = {handleUpdate}
+                        />
+                    ))}
+				</tbody>
+			</Table>
         </div>
     );
-
 }
 
 export default ProductsTable;
